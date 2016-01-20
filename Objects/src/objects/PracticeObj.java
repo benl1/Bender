3b2366724cda0c6557710e5b6ff0dfe379ac6f1d
Benjamin Levine <benjaminL12345@gmail.com>
2016-01-20 11:15:22 -0500

    update

--- Objects/src/objects/PracticeObj.java
+++ Objects/src/objects/PracticeObj.java
@@ -6,8 +6,24 @@ import objects.graphics.Gui;
 public class PracticeObj {
 	
 	public static void main(String[] args){
-		Gui gui = new Gui(3);
+		Gui gui = new Gui();
+		Bot b = gui.getBot(); //gets single bot on screen
+		//Ex. Bot b = gui.getBot(x), if using more than one bot, you can use getBot(int x) to get a reference to the xth bot on the screen (goes left to right) 
+		moveBot(b);
+		
+		/*The code above just starts up the gui and creates a single bot.
+		 * The constructor can also take in an integer and it will display however many bots are specified
+		 * I haven't put in animation yet so for now it just skips the bot instantly to whatever box it should be in
+		 * Moving is just moveRight, moveLeft ect..., these methods can also take an int to take more than one step at once
+		 * FUN SIDE NOTE*: The arrow keys can be used to move the bot.
+		 * Also*, if you choose to have more than one bot, click on whichever bot you want to move and the arrow keys will then control that bot
+		 *  
+		 */
+		
 	}
 	
+	private static void moveBot(Bot b){
+		
+	}
 	
 }
